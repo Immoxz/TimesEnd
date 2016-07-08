cf0b57f5bbe853b399ee3fbdc6b4e839ecf46ce2
ImmoXZ <things.immo@gmail.com>
2016-07-08 15:02:17 +0200

    add some functionality but still cant split main fxml

--- src/pl/immoxz/main/Controller.java
+++ src/pl/immoxz/main/Controller.java
@@ -1,6 +1,6 @@
 package pl.immoxz.main;
 
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -16,61 +16,95 @@ public class Controller implements Initializable {
     BetterTimeCounter betterTimeCounter = new BetterTimeCounter();
 
     int finalTime = 0;
-    int H = 0;
-    int M = 0;
-    int S = 0;
+    ConfirmBox confirmBox = new ConfirmBox();
+    AlertBox alertBox = new AlertBox();
 
     @FXML
-    private TextField afterHouSet;
+    private TextField afterHouSet, afterMinSet, afterSecSet, hSet, mSet, sSet;
     @FXML
-    private TextField afterMinSet;
+    private Label curTime, curBotTime;
     @FXML
-    private TextField afterSecSet;
+    private RadioButton toggleOnTime = new RadioButton(), toggleAfterTime = new RadioButton();
     @FXML
-    private Label curTime;
-    @FXML
-    private Label curBotTime;
-    @FXML
-    private TextField hSet;
-    @FXML
-    private TextField mSet;
-    @FXML
-    private TextField sSet;
+    private Button onTimeButton, buttonAfterTime;
 
 
     //TODO stwoprzyc alertbox na elsa gdy nie ustawiono godziny
+    @FXML
     public void okButtonClicked() {
         System.out.println("ok button");
-        if (finalTime <= 60)
-            execCommand("shutdown /f /s /t " + this.finalTime);
-        //else
+        if (finalTime >= 60) {
+            System.out.println("zamyka za 60++");
+//            execCommand("shutdown /f /s /t " + this.finalTime);
+        } else {
+            if (confirmBox.displey("Wykryto brak wprowadzonego czasu zamknięcia", "Czy chcesz zamknąc system teraz?")) {
+                alertBox.displey("Zamykanie Systemu", "Za 60 sekund system zostanie zamknięty!");
+//                execCommand("shutdown /f /s /t " + 60);
+            }
+        }
         System.out.println(this.finalTime);
     }
 
+    @FXML
+    public void onToggleButtonOn() {
+            hSet.setDisable(true);
+            mSet.setDisable(true);
+            sSet.setDisable(true);
+            onTimeButton.setDisable(true);
+
+            afterHouSet.setDisable(false);
+            afterMinSet.setDisable(false);
+            afterSecSet.setDisable(false);
+            buttonAfterTime.setDisable(false);
+    }
+    @FXML
+    public void onToggleButtonOff() {
+            afterHouSet.setDisable(true);
+            afterMinSet.setDisable(true);
+            afterSecSet.setDisable(true);
+            buttonAfterTime.setDisable(true);
+
+            hSet.setDisable(false);
+            mSet.setDisable(false);
+            sSet.setDisable(false);
+            onTimeButton.setDisable(false);
+    }
+
+    @FXML
     public void cancelButtonClicked() {
         System.out.println("cancel button");
         execCommand("shutdown -a");
     }
 
+
     @FXML
     public void setTime() {
-
-
-        if (afterHouSet.getText() != null)
+        int H = 0, M = 0, S = 0;
+        if (!afterHouSet.getText().isEmpty()) {
             H = Integer.parseInt(afterHouSet.getText());
-        if (afterMinSet.getText() != null)
+        }
+        if (!afterMinSet.getText().isEmpty()) {
             M = Integer.parseInt(afterMinSet.getText());
-        if (afterSecSet.getText() != null)
-            S = Integer.parseInt(afterSecSet.getText());
+        }
+        if (!afterSecSet.getText().isEmpty()) {
+            H = Integer.parseInt(afterSecSet.getText());
+        }
         System.out.println(afterHouSet.getText() + " " + afterMinSet.getText() + " " + afterSecSet.getText());
         System.out.println(H + " " + M + " " + S);
         this.finalTime = betterTimeCounter.cauntTime(H, M, S);
     }
 
+
     public void testRead() {
-        System.out.println(hSet.getText() + " " + mSet.getText() + " " + sSet.getText()+ " test");
-        int[] cos = betterTimeCounter.countDiff("12","50","00");
+        System.out.println(hSet.getText() + " " + mSet.getText() + " " + sSet.getText() + " test");
+        int[] cos = betterTimeCounter.countDiff("12", "50", "00");
+        if (confirmBox.displey("Hello", "chcesz przetestować to?")) {
+            System.out.println("działa");
+        } else {
+            System.out.println("nie działa");
+        }
     }
+
 
     public void execCommand(String cmd) {
         ProcessBuilder builder = new ProcessBuilder(
@@ -96,9 +130,17 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        toggleAfterTime.setSelected(true);
         curBotTime.setText(betterTimeCounter.takeCurrentTime());
         hSet.setText(betterTimeCounter.takeCurrentHour());
         mSet.setText(betterTimeCounter.takeCurrentMinute());
         sSet.setText(betterTimeCounter.takeCurrentSec());
+        if (!toggleOnTime.isSelected()) {
+            hSet.setDisable(true);
+            mSet.setDisable(true);
+            sSet.setDisable(true);
+            onTimeButton.setDisable(true);
+        }
+
     }
 }
--- src/pl/immoxz/main/Main.java
+++ src/pl/immoxz/main/Main.java
@@ -4,15 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("setTime.fxml"));
+        BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("setTime.fxml"));
+        /*BorderPane wholeScene = new BorderPane();
+
+        VBox center =  FXMLLoader.load(getClass().getResource("centerOneScene.fxml"));
+        HBox top = new HBox(FXMLLoader.load(getClass().getResource("topScene.fxml")))
+                ,bot = new HBox(FXMLLoader.load(getClass().getResource("botScene.fxml")));
+
+        wholeScene.setTop(top);
+        wholeScene.setCenter(center);
+        wholeScene.setBottom(bot);*/
+
         primaryStage.setTitle("Time is End");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 600, 300));
         primaryStage.show();
     }
 
